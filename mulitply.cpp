97ad2d9f6765b64bd923cae0579647a139e31e07
ChennuriAkhilvarma <85404631+ChennuriAkhilvarma@users.noreply.github.com>
2021-06-07 11:06:08 +0530

    product.cpp

---
+++ mulitply.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+  float a,b;
+  float prod;
+  cout<<"enter two numbers:";
+  cin>>a>>b;
+  prod=a*b;
+  cout<<"product="<<prod;
+  
+  return 0;
+}
+  
